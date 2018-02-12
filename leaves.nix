912f0cc9855bd549b16f71ff5deffa03f28a4b9b
Colin King <colin@mercury.co>
2018-02-12 12:10:41 -0600

    Initial

---
+++ leaves.nix
@@ -0,0 +1,16 @@
+{ prsJSON, nixpkgs }:
+let
+  util = import ./util.nix;
+  pkgs = import nixpkgs {};
+  prs = builtins.fromJSON (builtins.readFile prsJSON);
+  prJobsets = pkgs.lib.listToAttrs (pkgs.lib.mapAttrsToList util.makePR (prs "leaves"));
+  mainJobsets = with pkgs.lib; mapAttrs (name: settings: util.defaultSettings // settings) (rec {
+    leaves = util.mkProject "leaves" "master" util.nixpkgs-src.rev;
+  });
+  jobsetsAttrs = prJobsets // mainJobsets;
+  jobsetJson = pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs);
+in {
+  jobsets = with pkgs.lib; pkgs.runCommand "spec.json" {} ''
+    cp ${jobsetJson} $out
+  '';
+}
---
+++ util.nix
@@ -0,0 +1,40 @@
+let
+  mkFetchGithub = value: {
+    inherit value;
+    type = "git";
+    emailresponsible = false;
+  };
+in rec {
+  nixpkgs-src = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+  defaultSettings = {
+    enabled = 1;
+    hidden = false;
+    nixexprinput = "jobsets";
+    keepnr = 5;
+    schedulingshares = 42;
+    checkinterval = 60;
+    enableemail = false;
+    emailoverride = "";
+  };
+  mkProject = project: branch: nixpkgsRev: {
+    nixexprpath = "release.nix";
+    nixexprinput = "src";
+    description = project;
+    inputs = {
+      src = mkFetchGithub "https://github.com/hakujin/${project}.git ${branch}";
+      nixpkgs = mkFetchGithub "https://github.com/NixOS/nixpkgs.git ${nixpkgs-src.rev}";
+    };
+  };
+  makePR = project: num: info: {
+    name = "${project}-pr-${num}";
+    value = defaultSettings // {
+      description = "PR ${num}: ${info.title}";
+      nixexprinput = "src";
+      nixexprpath = "release.nix";
+      inputs = {
+        nixpkgs = mkFetchGithub "https://github.com/NixOS/nixpkgs.git master";
+        src = mkFetchGithub "https://github.com/${info.head.repo.owner.login}/${info.head.repo.name}.git ${info.head.ref}";
+      };
+    };
+  };
+}
